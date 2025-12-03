19e5b5ca7bbea3e8bbbcaa513816981481ff08a9
MaikLangel0 <mikaellunde2003@hotmail.com>
2025-12-03 09:45:28 +0100

    rem solve_first as it was redundant

--- src/day_3/day_3.rs
+++ src/day_3/day_3.rs
@@ -1,11 +1,14 @@
 use std::{fs::File, io::{BufRead, BufReader}};
+use std::time::{Instant};
 
 fn main() {
+    let start = Instant::now();
+
     let file = File::open("src\\day_3\\input.txt").unwrap();
     let reader = BufReader::new(&file);
 
-    let mut buf: [u8; _] = [0; 100];
-    let mut res_first: u16 = 0;
+    let mut buf: [u8; _] = [0; 100]; // CHANGE GIVEN INPUT SIZE YO
+    let mut res_first: u64 = 0;
     let mut res_second: u64 = 0;
 
     for line in reader.lines() {
@@ -15,50 +18,20 @@ fn main() {
         buf.copy_from_slice(line.as_bytes());
         buf = buf.map(|n| n - '0' as u8); // Converting from ascii representation of number to u8
 
-        solve_first(&buf, &mut res_first);
-        solve_second(&buf, 12, &mut res_second);
+        // solve_first(&buf, &mut res_first);
+        solve(&buf, 2, &mut res_first);
+        solve(&buf, 12, &mut res_second);
     }
+
+    let end = start.elapsed();
+    println!("TOTAL END TIME (μs): {}", end.as_micros());
 
     println!("RES 1: {}", res_first);
     println!("RES 2: {}", res_second);
 }
 
-fn solve_first(buf: &[u8; 100], res: &mut u16) {
-    //              pos|val
-    let mut first: (u8, u8) = (0, 1);
-    let mut second: u8 = 1;
-
-    let mut idx: u8 = 0;
-
-    for num in buf.iter().take( buf.len() - 1 ) {
-        idx += 1;
-
-        if *num == 9 {
-            first = (idx, 9);
-            break;
-        }
-
-        if *num > first.1 {
-            first = (idx, *num);
-        }
-    }
-
-    for num in buf.iter().skip(first.0 as usize) {
-        if *num == 9 {
-            second = 9;
-            break;
-        }
-
-        if *num > second {
-            second = *num;
-        }
-    }
-
-    *res += ( (first.1 * 10) + second ) as u16;
-}
-
 // General for all jolt sizes => jolt = 2 will give the same answer as solve_first()
-fn solve_second(buf: &[u8; 100], jolt_amount: usize, res: &mut u64) {
+fn solve(buf: &[u8], jolt_amount: usize, res: &mut u64) {
     debug_assert!(jolt_amount <= buf.len(), "Jolt amount cannot be bigger than buf len.");
 
     let mut idx_of_curr_jolt: u8 = 0;
@@ -70,8 +43,8 @@ fn solve_second(buf: &[u8; 100], jolt_amount: usize, res: &mut u64) {
 
         //             pos|val
         let mut curr: (u8, u8) = (0, 1);
-
         let mut idx: u8 = 0;
+
         for num in buf_iter {
             idx += 1;
 
