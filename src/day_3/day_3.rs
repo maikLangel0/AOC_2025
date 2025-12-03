9145b3ed4334d68a3507a3e23e6bd77b2a9b06cb
MaikLangel0 <mikaellunde2003@hotmail.com>
2025-12-03 08:48:27 +0100

    omE

--- src/day_3/day_3.rs
+++ src/day_3/day_3.rs
@@ -80,8 +80,7 @@ fn solve_second(buf: &[u8; 100], jolt_amount: usize, res: &mut u64) {
             }
 
             if curr.1 < *num {
-                curr.1 = *num;
-                curr.0 = idx;
+                curr = (*num, idx);
             }
         }
 
